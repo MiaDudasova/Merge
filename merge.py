306daad77bc7ac91f7ef763cc024b0862ff03f44
MiaDudasova <91089971+MiaDudasova@users.noreply.github.com>
2021-10-13 10:18:59 +0200

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,3 +1,9 @@
+def divide(a):
+    stred = int(len(a)/2)
+    pole1 = a[:stred]
+    pole2 = a[stred:]
+    return pole1, pole2
+
 def merge(a, b):
     posledne_a = 0
     posledne_b = 0
@@ -23,7 +29,23 @@ def merge(a, b):
             posledne_b += 1
     return c
 
+def divide_merge(a):
+    x,y = divide(a)
+    if len(x) > 2:
+        x = divide_merge(x)
+    elif len(x) == 2:
+        if x[0] > x[1]:
+            num1 = x[0]
+            x[0] = x[1]
+            x[1] = num1
+    if len(y) > 2:
+        y = divide_merge(y)
+    elif len(y) == 2:
+        if y[0] > y[1]:
+            num1 = y[0]
+            y[0] = y[1]
+            y[1] = num1
+    return merge(x, y)
 
-a = [1, 3, 5, 8]
-b = [2, 4, 6, 7]
-print(merge(a, b))
+a = [1, 3, 5, 8, 2, 4, 6, 7]
+print(divide_merge(a))
